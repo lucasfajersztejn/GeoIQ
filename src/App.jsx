3ea5e34aaf05bec8a3567a30e2043cede5d0a878
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-11-26 12:14:53 +0100

    Project Initialized

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section>
+        <header>
+
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+      </section>
+    </>
+  )
+}
+
+export default App
