a17acc3204a1525313d7b46e1431effddf3ded88
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-11-26 12:35:25 +0100

    Working in the home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link } from 'react-router'
 
 
 function App() {
@@ -6,16 +7,17 @@ function App() {
 
   return (
     <>
-      <section>
-        <header>
+      <section className='flex flex-col h-svh'>
+        <header className='bg-emerald-400 h-72'>
 
         </header>
 
-        <main>
-
+        <main className='bg-blue-600 h-48 flex justify-center items-center gap-5'>
+          <div className='border-cyan-400 border-4'>Grabar encuesta</div>
+          <div className='border-cyan-400 border-4'>Seguimiento de encuestas</div>
         </main>
 
-        <footer>
+        <footer className='bg-red-500 h-72'>
 
         </footer>
       </section>
