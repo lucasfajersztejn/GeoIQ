59b8802a9244b18a5c6bc783cb16f1c65144faf7
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2025-02-03 14:57:39 +0100

    Adding more devices

--- web/src/pages/Devices.jsx
+++ web/src/pages/Devices.jsx
@@ -16,6 +16,24 @@ import PrimeBeige from '../assets/images/Prime/Beige.png';
 import PrimeGreen from '../assets/images/Prime/Green.png';
 import PrimeBlack from '../assets/images/Prime/Black.png';
 import PrimeBronze from '../assets/images/Prime/Gris Lunar.png';
+import oneAoki from '../assets/images/E limit/One_AOKI.png';
+import ilumaAoki from '../assets/images/E limit/Iluma_AOKI.png';
+import primeAoki from '../assets/images/E limit/Prime_AOKI.png';
+import oneStarterKit from '../assets/images/Veev one/One starter kit.png';
+import oneSilkyGrey from '../assets/images/Veev one/One grey.png';
+import oneVelvetBlack from '../assets/images/Veev one/One black.png';
+import oneLusciousPink from '../assets/images/Veev one/One pink.png';
+import oneFreshyGreen from '../assets/images/Veev one/One green.png';
+import oneElectricPurple from '../assets/images/Veev one/One purple.png';
+import vUltraBlueRaspberry from '../assets/images/Veev ultra/Blue raspberry.png';
+import vUltraClassicMint from '../assets/images/Veev ultra/Classic Mint.png';
+import vUltraLemonLime from '../assets/images/Veev ultra/Lemon lime.png';
+import vUltraMango from '../assets/images/Veev ultra/Mango.png';
+import vUltraPassionFruit from '../assets/images/Veev ultra/Passion fruit kiwi guava.png';
+import vUltraStrawberry from '../assets/images/Veev ultra/Strawberry.png';
+import vUltraWatermelon from '../assets/images/Veev ultra/Watermelon.png';
+
+
 
 
 
@@ -129,21 +147,6 @@ const Devices = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-5 gap-4 mt-4">
         <Card 
-          image={IlumaBlue}
-          title="Iluma MK Azure Blue" 
-          onCountChange={(count) => handleDeviceCountChange('Iluma MK Azure Blue', count)}
-        />
-        <Card 
-          image={IlumaBeige}º
-          title="Iluma MK Pebble Beige" 
-          onCountChange={(count) => handleDeviceCountChange('Iluma MK Pebble Beige', count)}
-        />
-        <Card 
-          image={IlumaGreen}º
-          title="Iluma MK Moss Green" 
-          onCountChange={(count) => handleDeviceCountChange('Iluma MK Moss Green', count)}
-        />
-        <Card 
           image={IlumaGrey}º
           title="Iluma MK Pebble Grey" 
           onCountChange={(count) => handleDeviceCountChange('Iluma MK Pebble Grey', count)}
@@ -153,19 +156,24 @@ const Devices = () => {
           title="Iluma MK Sunset Red" 
           onCountChange={(count) => handleDeviceCountChange('Iluma MK Sunset Red"', count)}
         />
+        <Card 
+          image={IlumaGreen}º
+          title="Iluma MK Moss Green" 
+          onCountChange={(count) => handleDeviceCountChange('Iluma MK Moss Green', count)}
+        />
+        <Card 
+          image={IlumaBlue}
+          title="Iluma MK Azure Blue" 
+          onCountChange={(count) => handleDeviceCountChange('Iluma MK Azure Blue', count)}
+        />
+        <Card 
+          image={IlumaBeige}º
+          title="Iluma MK Pebble Beige" 
+          onCountChange={(count) => handleDeviceCountChange('Iluma MK Pebble Beige', count)}
+        />
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4 mt-4">
-        <Card 
-          image={PrimeBeige}
-          title="Iluma Prime Golden Khaki" 
-          onCountChange={(count) => handleDeviceCountChange('Iluma Prime Golden Khaki', count)}
-        />
-        <Card 
-          image={PrimeGreen}º
-          title="Iluma Prime Jade Green" 
-          onCountChange={(count) => handleDeviceCountChange('Iluma Prime Jade Green', count)}
-        />
         <Card 
           image={PrimeBlack}º
           title="Iluma Prime Obsidian Black" 
@@ -176,8 +184,106 @@ const Devices = () => {
           title="Iluma Prime Bronze Taupe" 
           onCountChange={(count) => handleDeviceCountChange('Iluma Prime Bronze Taupe', count)}
         />
+        <Card 
+          image={PrimeGreen}º
+          title="Iluma Prime Jade Green" 
+          onCountChange={(count) => handleDeviceCountChange('Iluma Prime Jade Green', count)}
+        />
+        <Card 
+          image={PrimeBeige}
+          title="Iluma Prime Golden Khaki" 
+          onCountChange={(count) => handleDeviceCountChange('Iluma Prime Golden Khaki', count)}
+        />
       </div>
 
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-4">
+        <Card 
+          image={oneAoki}º
+          title="Iluma One AOKI Edition" 
+          onCountChange={(count) => handleDeviceCountChange('Iluma One AOKI Edition"', count)}
+        />
+        <Card 
+          image={ilumaAoki}º
+          title="Iluma AOKI Edition" 
+          onCountChange={(count) => handleDeviceCountChange('Iluma AOKI Edition', count)}
+        />
+        <Card 
+          image={primeAoki}º
+          title="Iluma Prime AOKI Edition" 
+          onCountChange={(count) => handleDeviceCountChange('Iluma Prime AOKI Edition"', count)}
+        />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-4 mt-4">
+        <Card 
+          image={oneStarterKit}º
+          title="VEEV ONE Starter Kit" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ONE Starter Kit', count)}
+        />
+        <Card 
+          image={oneSilkyGrey}º
+          title="VEEV ONE Silky Grey" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ONE Silky Grey', count)}
+        />
+        <Card 
+          image={oneVelvetBlack}º
+          title="VEEV ONE Velvet Black" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ONE Velvet Black', count)}
+        />
+        <Card 
+          image={oneLusciousPink}º
+          title="VEEV ONE Luscious Pink" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ONE Luscious Pink', count)}
+        />
+        <Card 
+          image={oneFreshyGreen}º
+          title="VEEV ONE Freshy Green" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ONE Freshy Green', count)}
+        />
+        <Card 
+          image={oneElectricPurple}º
+          title="VEEV ONE Electric Purple" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ONE Electric Purple', count)}
+        />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+        <Card 
+          image={vUltraBlueRaspberry}º
+          title="VEEV ULTRA Blue Raspberry" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ULTRA Blue Raspberry', count)}
+        />
+        <Card 
+          image={vUltraClassicMint}º
+          title="VEEV ULTRA Classic Mint" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ULTRA Classic Mint', count)}
+        />
+        <Card 
+          image={vUltraLemonLime}º
+          title="VEEV ULTRA Lemon Lime" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ULTRA Lemon Lime', count)}
+        />
+        <Card 
+          image={vUltraMango}º
+          title="VEEV ULTRA Mango" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ULTRA Mango', count)}
+        />
+        <Card 
+          image={vUltraPassionFruit}º
+          title="VEEV ULTRA Passion Fruit Kiwi Guava" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ULTRA Passion Fruit Kiwi Guava', count)}
+        />
+        <Card 
+          image={vUltraStrawberry}º
+          title="VEEV ULTRA Strawberry" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ULTRA Strawberry', count)}
+        />
+        <Card 
+          image={vUltraWatermelon}º
+          title="VEEV ULTRA Watermelon" 
+          onCountChange={(count) => handleDeviceCountChange('VEEV ULTRA Watermelon', count)}
+        />
+      </div>
 
       <h2 className="text-center text-2xl font-bold mt-8 mb-4">Accessories</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -193,7 +299,7 @@ const Devices = () => {
         />
         {/* Agrega más tarjetas de accesorios según sea necesario */}
       </div>
-      <div className="flex space-x-4 mt-8 justify-center">
+      <div className="flex space-x-4 mt-8 justify-center my-6">
         <button
           onClick={handleBack}
           className="w-32 h-12 bg-cyanCustom text-white font-semibold rounded-md shadow-md transition-all hover:bg-cyan-500"
